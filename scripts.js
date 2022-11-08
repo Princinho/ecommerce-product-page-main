0f58cbbd0aad603498e707ecf6feb6b78a7d8aae
Princinho <princegnakou@gmail.com>
2022-11-08 11:27:07 +0000

    add lightbox carousel

--- scripts.js
+++ scripts.js
@@ -1,26 +1,39 @@
 const mainNav = document.getElementById('main-nav')
 const nextButton = document.getElementById('btn-next')
 const prevButton = document.getElementById('btn-prev')
-const images = [...document.querySelectorAll('.product-image')]
+const images = [...document.querySelectorAll('.main-carousel .product-image')]
 const thumbnails = [...document.querySelectorAll('.product-thumbnail')]
 const quantityInput = document.getElementById('quantity')
 const cartContainer = document.getElementById('cart-container')
 const cartItemsList = document.getElementById('cart-items-list')
 const quantityTag = document.getElementById('quantity-tag')
 const checkoutBtn = document.getElementById('btn-checkout')
+const lightbox = document.getElementById('lightbox')
+
+
+const lightboxNextButton = document.getElementById('btn-next-lightbox')
+const lightboxPrevButton = document.getElementById('btn-prev-lightbox')
+const lightboxImages = [...document.querySelectorAll('.lightbox-carousel .product-image')]
+const lightboxThumbnails = [...document.querySelectorAll('.lightbox-carousel .product-thumbnail')]
+
+
 const emptyCartMessage = document.getElementById('empty-cart-message')
 const productPrice = 250;
 const discountPercentage = 50;
 let currentImageIndex = 0
 let quantity = 0
 let cartQuantity = 0
+
+
+
+
 function toggleNav() {
     mainNav.classList.toggle('visible')
 }
 
 function setCurrentImage(index) {
     images.map((image, imgIndex) => {
-        if (index < imgIndex) {
+        if (imgIndex < index) {
             image.classList.add('viewed')
         }
         else {
@@ -40,10 +53,12 @@ function setCurrentImage(index) {
 
         }
     })
+    currentImageIndex = index;
+    updateButtonsState()
 
 }
 
-function showNextImage() {
+function showNextImage(target = '') {
     //hide current image
     images[currentImageIndex].classList.remove('current')
     images[currentImageIndex].classList.add('viewed')
@@ -51,7 +66,15 @@ function showNextImage() {
     //display next image
     images[currentImageIndex + 1].classList.toggle('current')
     thumbnails[currentImageIndex + 1].classList.toggle('current')
-
+    // if (target) {
+        //hide current image
+        lightboxImages[currentImageIndex].classList.remove('current')
+        lightboxImages[currentImageIndex].classList.add('viewed')
+        lightboxThumbnails[currentImageIndex].classList.remove('current')
+        //display next image
+        lightboxImages[currentImageIndex + 1].classList.toggle('current')
+        lightboxThumbnails[currentImageIndex + 1].classList.toggle('current')
+    // }
     currentImageIndex += 1
     updateButtonsState()
     console.log(currentImageIndex, images.length)
@@ -61,7 +84,7 @@ function addToCart() {
     quantity = 0
     render()
 }
-function showPrevImage() {
+function showPrevImage(target = "") {
     //hide current image
     images[currentImageIndex].classList.remove('current')
     thumbnails[currentImageIndex].classList.remove('current')
@@ -70,15 +93,26 @@ function showPrevImage() {
     //display next image
     images[currentImageIndex - 1].classList.add('current')
     thumbnails[currentImageIndex - 1].classList.add('current')
-
-
+    // if (target) {
+        lightboxImages[currentImageIndex].classList.remove('current')
+        lightboxThumbnails[currentImageIndex].classList.remove('current')
+        lightboxImages[currentImageIndex - 1].classList.remove('viewed')
+        // images[currentImageIndex].classList.toggle('viewed')
+        //display next image
+        lightboxImages[currentImageIndex - 1].classList.add('current')
+        lightboxThumbnails[currentImageIndex - 1].classList.add('current')
+    // }
     currentImageIndex -= 1
     updateButtonsState()
     console.log(currentImageIndex, images.length)
 }
+
 function updateButtonsState() {
+    console.log(currentImageIndex)
     prevButton.disabled = currentImageIndex === 0;
-    nextButton.disabled = currentImageIndex === images.length - 1;
+    nextButton.disabled = currentImageIndex >= images.length - 1;
+    lightboxPrevButton.disabled = currentImageIndex === 0;
+    lightboxNextButton.disabled = currentImageIndex >= images.length - 1;
 }
 function changeQuantity(amount) {
     quantity += amount
@@ -144,5 +178,14 @@ thumbnails.map((thumb, index) => {
         setCurrentImage(index)
     })
 })
+function showLightbox(){
+    lightbox.classList.toggle('visible')
+
+}
+images.map(image => image.addEventListener('click', () => showLightbox()))
+
+function closeLightbox() {
+    lightbox.classList.toggle('visible')
+}
 
 render()
